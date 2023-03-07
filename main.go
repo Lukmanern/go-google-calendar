3e7399d3fe1bb97be99cc36faedda5eef707ff83
LukmanE22 <unsurlukman@gmail.com>
2023-03-07 11:14:18 +0700

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	// Set up the Google Calendar API client
+	ctx := context.Background()
+	service, err := calendar.NewService(ctx, option.WithCredentialsFile("./credentials.json"))
+	if err != nil {
+		log.Fatalf("Unable to create Calendar service: %v", err)
+	}
+
+	// Create a new event
+	event := &calendar.Event{
+		Summary:     "My Event",
+		Description: "This is an example event",
+		Start: &calendar.EventDateTime{
+			DateTime: time.Now().Format(time.RFC3339),
+			TimeZone: "America/Los_Angeles",
+		},
+		End: &calendar.EventDateTime{
+			DateTime: time.Now().Add(time.Hour).Format(time.RFC3339),
+			TimeZone: "America/Los_Angeles",
+		},
+	}
+
+	// Add the event to the calendar
+	calendarID := "primary"
+	event, err = service.Events.Insert(calendarID, event).Do()
+	if err != nil {
+		log.Fatalf("Unable to create event: %v", err)
+	}
+	fmt.Printf("Event created: %s\n", event.HtmlLink)
+}
